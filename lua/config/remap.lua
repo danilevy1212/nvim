b321f24faa0bb04d41ea167d5c4058e5bb6fd957
Daniel Levy Moreno <daniellevymoreno@gmail.com>
2023-01-04 21:02:50 -0500

    feat(remap): Keep search jumps centered

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -33,3 +33,11 @@ setk('n', '<leader>fY', function()
     local project_file_path = vim.fn.expand '%:p:.'
     set_clipboard(project_file_path)
 end, { desc = 'Copy project filepath to clipboard' })
+
+-- Keep jumps centered
+setk('n', '<C-u>', '<C-u>zz', { desc = 'Jump half-page up' })
+setk('n', '<C-d>', '<C-d>zz', { desc = 'Jump half-page down' })
+setk('n', '<C-b>', '<C-u>zz', { desc = 'Jump page up' })
+setk('n', '<C-f>', '<C-f>zz', { desc = 'Jump page down' })
+setk('n', 'n', 'nzzzv', { desc = 'Repeat last pattern' })
+setk('n', 'N', 'Nzzzv', { desc = 'Repeat last pattern backwards'})
