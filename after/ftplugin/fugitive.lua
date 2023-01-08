043118173a2a9475d378a22b11ef1fd0bb2bf108
Daniel Levy Moreno <daniellevymoreno@gmail.com>
2023-01-08 16:18:12 -0500

    feat(fugitive): No relative number

--- after/ftplugin/fugitive.lua
+++ after/ftplugin/fugitive.lua
@@ -4,3 +4,7 @@ vim.keymap.set({ 'n', 'v', 'x' }, '<Tab>', '=', {
     remap = true,
     desc = 'Toggle fold',
 })
+
+-- No number fringe
+vim.opt_local.number = false
+vim.opt_local.relativenumber = false
