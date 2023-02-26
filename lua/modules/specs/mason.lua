e52ecc3e055de86d30d9ae06b1cdea46f546005a
Daniel Levy Moreno <dalevy@autopay.com>
2023-02-25 20:12:12 -0500

    chore(refactor): Removed mason-lspconfig

--- lua/modules/specs/mason.lua
+++ lua/modules/specs/mason.lua
@@ -8,50 +8,38 @@ return {
         require('mason').setup()
 
         -- Packages to install
-        local package_names = {}
-
-        -- For lua (on MacOS)
-        if CONSTANTS.IS_MACOS then
-            package_names[#package_names + 1] = 'stylua'
-        end
+        local package_names = {
+            -- For lua
+            'lua-language-server',
+            'stylua',
+            -- For nix
+            'nil',
+            -- For JSON
+            'json-lsp',
+            -- For JS / TS
+            'typescript-language-server',
+            'eslint-lsp',
+            -- For rust
+            'rust-analyzer',
+            -- For docker
+            'dockerfile-language-server',
+        }
 
         -- Install packages if not present
         for _, package_name in ipairs(package_names) do
-            local package = require('mason-registry').get_package(package_name)
-            if not package:is_installed() then
-                package:install()
+            local is_ok, package = pcall(require('mason-registry').get_package, package_name)
+
+            if not is_ok then
+                vim.notify(
+                    'Mason package ' .. package_name .. ' was not found in the registry',
+                    vim.log.levels.WARN,
+                    {}
+                )
+            else
+                if not package:is_installed() then
+                    package:install()
+                end
             end
         end
     end,
-    requires = {
-        -- LSP server installer helper
-        {
-            'williamboman/mason-lspconfig.nvim',
-            after = 'mason.nvim',
-            config = function()
-                local ensure_installed = {
-                    -- For JSON
-                    'jsonls',
-                    -- For JS / TS
-                    'tsserver',
-                    'eslint',
-                    -- For VimL (Yikes!)
-                    'vimls',
-                    -- For nix
-                    'nil_ls',
-                    -- For rust
-                    'rust_analyzer',
-                }
-
-                -- For lua (on MacOS)
-                if CONSTANTS.IS_MACOS then
-                    ensure_installed[#ensure_installed + 1] = 'sumneko_lua'
-                end
-
-                require('mason-lspconfig').setup {
-                    ensure_installed = ensure_installed,
-                }
-            end,
-        },
-    },
 }
