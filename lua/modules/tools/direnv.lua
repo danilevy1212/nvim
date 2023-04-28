c361eff78d0dde36dfd90d3e471f4742e1a0e8dc
Daniel Levy Moreno <daniellevymoreno@gmail.com>
2023-04-28 08:54:11 -0400

    knit: Better dependency handling

--- lua/modules/tools/direnv.lua
+++ lua/modules/tools/direnv.lua
@@ -2,12 +2,9 @@
 return {
     'direnv/direnv.vim',
     event = 'DirChangedPre',
+    --- Load `mason` before direnv changes the value of `PATH`
+    dependencies = { 'williamboman/mason.nvim' },
     config = function()
-        --- Load `mason` before direnv changes the value of `PATH`
-        if not package.loaded['mason'] then
-            require 'mason'
-        end
-
         -- Use 'bash' parser for 'direnv' filetype
         vim.treesitter.language.register('bash', 'direnv')
     end,
--- lua/modules/tools/lsp.lua
+++ lua/modules/tools/lsp.lua
@@ -8,5 +8,7 @@ return {
         },
         -- LSP Completion
         { 'hrsh7th/cmp-nvim-lsp' },
+        --- Load `mason` before trying to intialize the lsp servers
+        { 'williamboman/mason.nvim' },
     },
 }
