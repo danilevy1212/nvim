806d064a23ea21d67cd52f2f9063524f91d14e28
Daniel Levy Moreno <daniellevymoreno@gmail.com>
2023-04-28 08:53:35 -0400

    feat(lualine): A better UI

---
+++ lua/modules/ui/lualine.lua
@@ -0,0 +1,64 @@
+--- A blazing fast and easy to configure Neovim statusline written in Lua.
+
+return {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons', 'shaunsingh/nord.nvim' },
+    event = { 'UIEnter' },
+    config = function()
+        local filename_component = {
+            'filename',
+            --- Display new file status
+            newfile_status = true,
+            --- Display relative path
+            path = 1,
+        }
+
+        local opts = {
+            options = {
+                icons_enabled = true,
+                theme = 'nord',
+                component_separators = { left = '', right = '' },
+                section_separators = { left = '', right = '' },
+                disabled_filetypes = {
+                    statusline = {},
+                    winbar = {},
+                },
+                ignore_focus = {},
+                always_divide_middle = true,
+                globalstatus = false,
+                refresh = {
+                    statusline = 1000,
+                    tabline = 1000,
+                    winbar = 1000,
+                },
+            },
+            sections = {
+                lualine_a = { 'mode' },
+                lualine_b = { 'branch', 'diff', 'diagnostics' },
+                lualine_c = {
+                    filename_component,
+                },
+                lualine_x = { 'encoding', 'fileformat', 'filetype' },
+                lualine_y = { 'progress' },
+                lualine_z = { 'location' },
+            },
+            inactive_sections = {
+                lualine_a = {},
+                lualine_b = {},
+                lualine_c = { filename_component },
+                lualine_x = { 'location' },
+                lualine_y = {},
+                lualine_z = {},
+            },
+            tabline = {},
+            winbar = {},
+            inactive_winbar = {},
+            extensions = {
+                'toggleterm',
+                'fugitive',
+            },
+        }
+
+        require('lualine').setup(opts)
+    end,
+}
--- lua/modules/ui/nord.lua
+++ lua/modules/ui/nord.lua
@@ -2,7 +2,6 @@
 
 return {
     'shaunsingh/nord.nvim',
-    event = { 'UIEnter' },
     config = function()
         require('nord').set()
     end,
