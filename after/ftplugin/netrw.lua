29b2cddf0d556498123fdc68fb6db903eda1bc9b
Daniel Levy Moreno <daniellevymoreno@gmail.com>
2023-05-03 10:41:09 -0400

    knit: Move `netrw` customizations to ftplugin

--- after/ftplugin/netrw.lua
+++ after/ftplugin/netrw.lua
@@ -1,2 +1,11 @@
 -- Don't show spaces in netrw
 vim.opt_local.list = false
+
+-- Don't show the banner
+vim.g.netrw_banner = false
+
+-- Keep the current directory the same as the browsing directory
+vim.g.netrw_keepdir = false
+
+-- By default, don't show hidden files. Can be toggled with `gh`
+vim.g.netrw_list_hide = [[\(^\|\s\s\)\zs\.\S\+]]
