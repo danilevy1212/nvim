b7eabda8671c583b1e7825e277cae298a6c4cfdd
Daniel Levy Moreno <dalevy@autopay.com>
2023-05-26 22:03:48 -0400

    feat(indent-blankline): Highlight indent-blankline

---
+++ lua/modules/ui/indent-blankline.lua
@@ -0,0 +1,12 @@
+--- Adds indentation guides to all lines (including empty lines).
+
+return {
+    'lukas-reineke/indent-blankline.nvim',
+    event = { 'BufRead' },
+    config = function()
+        require('indent_blankline').setup {
+            show_current_context = true,
+            show_current_context_start = true,
+        }
+    end,
+}
