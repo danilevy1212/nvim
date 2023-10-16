5269e0eb657851f3bb36e8407a13c2d809cc032f
Daniel Levy Moreno <daniellevymoreno@gmail.com>
2023-10-16 08:40:26 -0400

    feat(nvim-notify): Minor adjustments

--- lua/modules/ui/nvim-notify.lua
+++ lua/modules/ui/nvim-notify.lua
@@ -3,11 +3,21 @@
 return {
     'rcarriga/nvim-notify',
     lazy = false,
+    keys = {
+        {
+            '<leader>sN',
+            function()
+                require('telescope').extensions.notify.notify()
+            end,
+            desc = 'Notifications',
+        },
+    },
     config = function()
         require('notify').setup {
+            level = vim.log.levels.INFO,
             stages = 'fade_in_slide_out',
             fps = 60,
-            timeout = 1000,
+            timeout = 750,
         }
         vim.notify = require 'notify'
     end,
