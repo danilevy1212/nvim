ab54dc8bc6a047ab0ac873faa02c499dab2d3eb0
Daniel Levy Moreno <daniellevymoreno@gmail.com>
2023-11-03 16:22:30 -0400

    feat(vim-visual-multi): Small enhancements

--- lua/modules/editor/vim-visual-multi.lua
+++ lua/modules/editor/vim-visual-multi.lua
@@ -9,11 +9,15 @@ local M = {
         vim.api.nvim_set_var('VM_maps', {
             ['Find Under'] = '<M-d>',
             ['Find Subword Under'] = '<M-d>',
+            ['I Next'] = ''
         })
 
         g.VM_quit_after_leaving_insert_mode = 1
         g.VM_set_statusline = 0
         g.VM_silent_exit = 1
+        g.VM_theme = 'nord'
+        g.VM_highlight_matches = ''
+        g.VM_single_mode_maps = 0
     end,
     event = 'BufRead',
 }
