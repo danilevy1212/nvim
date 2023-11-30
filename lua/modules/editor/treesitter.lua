ea6086b295542d2924e04316d79e624fbada11bf
Daniel Levy Moreno <daniellevymoreno@gmail.com>
2023-11-30 08:32:39 -0500

    feat(rainbow-delimeters): Deprecated ts-rainbow

--- lua/modules/editor/treesitter.lua
+++ lua/modules/editor/treesitter.lua
@@ -1,20 +1,27 @@
----Disable criteria. None for now
----@param lang string -- Filetype
----@param bufnr number -- Buffer number
----@return boolean
----@diagnostic disable-next-line: unused-local
-local disable = function(lang, bufnr)
-    return false
-end
+--- Treesitter enhancements for Neovim
 
--- Treesitter support
-return {
+--- @type LazyPluginSpec
+local M = {
     'nvim-treesitter/nvim-treesitter',
     event = { 'BufRead' },
     build = ':TSUpdate',
     dependencies = {
         {
-            'HiPhish/nvim-ts-rainbow2',
+            --- This Neovim plugin provides alternating syntax highlighting (“rainbow parentheses”) for Neovim, powered by Tree-sitter.
+            'HiPhish/rainbow-delimiters.nvim',
+            init = function()
+                vim.g.rainbow_delimiters = {
+                    highlight = {
+                        'RainbowDelimiterCyan',
+                        'RainbowDelimiterViolet',
+                        'RainbowDelimiterGreen',
+                        'RainbowDelimiterBlue',
+                        'RainbowDelimiterOrange',
+                        'RainbowDelimiterRed',
+                        'RainbowDelimiterYellow',
+                    },
+                }
+            end
         },
     },
     config = function()
@@ -52,22 +59,25 @@ return {
                 'vim',
                 'vimdoc',
             },
-
+            ignore_install = {},
             -- Install parsers synchronously (only applied to `ensure_installed`)
             sync_install = false,
-
             -- If a parser is missing, force me to add it to the ensure_installed list, for documentation sake
             auto_install = false,
-
             highlight = {
                 -- Higlight by default
                 enable = true,
-                -- Disable when is too large
-                disable = disable,
+                ---Disable criteria. None for now
+                ---@param lang string -- Filetype
+                ---@param bufnr number -- Buffer number
+                ---@return boolean
+                ---@diagnostic disable-next-line: unused-local
+                disable = function(lang, bufnr)
+                    return false
+                end,
                 -- Don't relay on regex highlight
                 additional_vim_regex_highlighting = false,
             },
-
             -- Enable incremental selection
             incremental_selection = {
                 enable = true,
@@ -78,32 +88,13 @@ return {
                     node_decremental = 'grm',
                 },
             },
-
             -- Indentation based on treesitter for the = operator.
             indent = {
                 enable = true,
             },
-
-            -- Rainbow parens
-            rainbow = {
-                enable = true,
-                -- list of languages you want to disable the plugin for
-                disable = {},
-                -- Which query to use for finding delimiters
-                query = 'rainbow-parens',
-                -- Highlight the entire buffer all at once
-                strategy = require 'ts-rainbow.strategy.global',
-                -- Default highlight groups
-                hlgroups = {
-                    'TSRainbowCyan',
-                    'TSRainbowViolet',
-                    'TSRainbowGreen',
-                    'TSRainbowBlue',
-                    'TSRainbowOrange',
-                    'TSRainbowRed',
-                    'TSRainbowYellow',
-                },
-            },
+            modules = {},
         }
     end,
 }
+
+return M
