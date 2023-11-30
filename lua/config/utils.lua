eb6133b834efd0df73d413c66c38912ea0034e2c
Daniel Levy Moreno <daniellevymoreno@gmail.com>
2023-11-30 10:09:28 -0500

    feat(diagnostics): `<C-k>` for diagnostics

--- lua/config/utils.lua
+++ lua/config/utils.lua
@@ -62,7 +62,8 @@ M.on_attach = function(_, bufnr)
             R = { vim.lsp.buf.references, 'Find references' },
         },
         K = { vim.lsp.buf.hover, 'Hover' },
-        ['<C-k>'] = { vim.lsp.buf.signature_help, 'Signature Help' },
+        --- NOTE Technically, diagnostics are seperate from LSP, but so far I am always using them together.
+        ['<C-k>'] = { vim.diagnostic.open_float, 'Show line diagnostic\'s information' },
         ['<leader>'] = {
             c = {
                 name = 'Code',
