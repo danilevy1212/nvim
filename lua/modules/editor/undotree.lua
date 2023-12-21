1e9f6e9ec73709b80c5636f1ec22d1c8abe17129
Daniel Levy Moreno <daniellevymoreno@gmail.com>
2023-12-21 08:21:40 -0500

    feat(modules): add undotree module

---
+++ lua/modules/editor/undotree.lua
@@ -0,0 +1,21 @@
+-- Visualizes the undo history and makes it easy to browse and switch between different undo branches.
+
+---@type LazyPluginSpec
+local M = {
+    'mbbill/undotree',
+    cmd = { 'UndotreeToggle' },
+    keys = {
+        {
+            '<leader>bu',
+            '<Cmd>UndotreeToggle<CR>',
+            desc = 'Toggle undotree',
+        },
+    },
+    config = function ()
+        local g = vim.g
+        g.undotree_WindowLayout = 2
+        g.undotree_SetFocusWhenToggle = 1
+    end
+}
+
+return M
