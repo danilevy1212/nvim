40425228e345d738c1deb5c2b0ddbfc25942ef56
Daniel Levy <dalevy@autopay.com>
2024-02-29 09:49:30 -0500

    feat(js_ts): Error translation for JS/TS

--- lua/dan/plugins/specs/filetype/js_ts.lua
+++ lua/dan/plugins/specs/filetype/js_ts.lua
@@ -4,6 +4,13 @@
 local M = {
     'dan/js_ts',
     ft = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact' },
+    dependencies = {
+        --- Translates errors from the `tsserver` LSP server into a more human-readable format
+        {
+            'dmmulroy/ts-error-translator.nvim',
+            opts = {},
+        },
+    },
 }
 
 return M
