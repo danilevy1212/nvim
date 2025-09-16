679a250e5766015159cf401182a9c45baf568089
Daniel Levy <dalevy@autopay.com>
2025-09-16 15:06:53 -0700

    feat(treesitter): auto-start treesitter for filetypes with available parsers

--- lua/dan/plugins/specs/editor/treesitter.lua
+++ lua/dan/plugins/specs/editor/treesitter.lua
@@ -33,6 +33,27 @@ local M = {
 
         treesitter.setup()
         treesitter.install 'all'
+
+        -- Start treesitter if there is a parser for it.
+        local group = vim.api.nvim_create_augroup(CONSTANTS.AUGROUP_PREFIX .. 'treesitter', { clear = true })
+        vim.api.nvim_create_autocmd('FileType', {
+            group = group,
+            pattern = '*',
+            callback = function(args)
+                local bufnr = args.buf
+                local filetype = vim.bo[bufnr].filetype
+
+                if filetype == nil then
+                    return
+                end
+
+                local lang = vim.treesitter.language.get_lang(filetype)
+
+                if lang and vim.list_contains(treesitter.get_available(), lang) then
+                    vim.treesitter.start(bufnr, lang)
+                end
+            end,
+        })
     end,
 }
 
