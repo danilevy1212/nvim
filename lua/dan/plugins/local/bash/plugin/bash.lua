d8e10c9651e0684db3f4e199c04cd85e65830c02
danilevy1212 <danilevy1212@gmail.com>
2025-12-03 07:51:31 -0800

    feat(bash): add DAP configuration for bash debugging

---
+++ lua/dan/plugins/local/bash/plugin/bash.lua
@@ -0,0 +1,42 @@
+local MASON_PKGS_DIR = vim.fn.stdpath 'data' .. '/mason/packages/'
+local BASHDB_DIR = MASON_PKGS_DIR .. 'bash-debug-adapter/extension/bashdb_dir'
+
+require('dan.lib.mason').ensure_installed({
+    'bash-debug-adapter',
+}, function()
+    require('dap').adapters.bashdb = {
+        type = 'executable',
+        command = MASON_PKGS_DIR .. 'bash-debug-adapter/bash-debug-adapter',
+        name = 'bashdb',
+    }
+
+    require('dap').configurations.sh = {
+        {
+            name = 'Launch Bash Debugger on current file',
+            type = 'bashdb',
+            request = 'launch',
+            program = '${file}',
+            cwd = '${workspaceFolder}',
+            args = function()
+                return coroutine.create(function(dap_run_co)
+                    vim.ui.input({ prompt = 'Args (space-separated, empty for none): ' }, function(a)
+                        if a == '' or not a then
+                            coroutine.resume(dap_run_co, {})
+                        else
+                            coroutine.resume(dap_run_co, vim.split(a, '%s+'))
+                        end
+                    end)
+                end)
+            end,
+            pathBashdb = BASHDB_DIR .. '/bashdb',
+            pathBashdbLib = BASHDB_DIR,
+            pathBash = 'bash',
+            pathCat = 'cat',
+            pathMkfifo = 'mkfifo',
+            pathPkill = 'pkill',
+            env = {},
+            -- showDebugOutput = true,
+            -- trace = true,
+        },
+    }
+end)
---
+++ lua/dan/plugins/specs/filetype/bash.lua
@@ -0,0 +1,9 @@
+-- Scripting lengua franca
+
+---@type LazyPluginSpec
+local M = {
+    'dan/bash',
+    ft = { 'sh' },
+}
+
+return M
